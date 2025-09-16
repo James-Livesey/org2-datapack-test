0a6ffc639aff7fb3ee5451e069d5a782414e572f
James Livesey <jamesliveg@gmail.com>
2025-09-16 23:46:08 +0100

    Try to get top slot working

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,23 @@
 
 #include "datapack.h"
 
-#define PIN_SD 16, 15, 17, 14, 18, 13, 19, 12
-#define PIN_SMR 11
-#define PIN_SCLK 20
-#define PIN_SOE_B 10
-#define PIN_SS_B 21
-#define PIN_SPGM_B 22
+#define TOP_SLOT
+
+#ifdef TOP_SLOT
+    #define PIN_SD 15, 14, 13, 12, 4, 5, 6, 7
+    #define PIN_SMR 8
+    #define PIN_SCLK 11
+    #define PIN_SOE_B 3
+    #define PIN_SS_B 10
+    #define PIN_SPGM_B 21
+#else
+    #define PIN_SD 16, 15, 17, 14, 18, 13, 19, 12
+    #define PIN_SMR 11
+    #define PIN_SCLK 20
+    #define PIN_SOE_B 10
+    #define PIN_SS_B 21
+    #define PIN_SPGM_B 22
+#endif
 
 #define PIN_BUS {PIN_SD, PIN_SMR, PIN_SCLK, PIN_SOE_B, PIN_SS_B, PIN_SPGM_B}
 
